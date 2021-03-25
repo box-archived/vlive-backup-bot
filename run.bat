f6537ed894fc45e70f9ea641fcb44584e75833d1
box-archived <box.cassette@gmail.com>
2021-03-25 11:40:36 +0900

    Temp commit

--- run.bat
+++ run.bat
@@ -3,26 +3,32 @@ chcp 65001
 TITLE VLIVE-BACKUP-BOT
 CLS
 
+SET T1=기존 가상환경을 정리합니다.
+SET T2=가상환경을 생성합니다.
+SET T3=의존 프로그램을 설치합니다.
+SET T4=의존프로그램 설치를 완료했습니다.
+SET T5=프로그램을 실행합니다.
+
 IF EXIST venv (
-ECHO 기존 가상환경을 정리합니다.
+ECHO %T1%
 ECHO.
 rmdir /S /Q venv
 )
 
-ECHO 가상환경을 생성합니다.
+ECHO %T2%
 ECHO.
 python -m venv venv
 
-ECHO 의존 프로그램을 설치합니다.
+ECHO %T3%
 ECHO.
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 
 ECHO.
-ECHO 의존 프로그램 설치를 완료했습니다.
+ECHO %T4%
 ECHO.
 
-ECHO 프로그램을 실행합니다.
+ECHO %T5%
 ECHO.
 TIMEOUT /t 1 > nul
 cls
