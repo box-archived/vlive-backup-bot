a3f9433383afa2e40fc32866721b3564de1b5e1e
box-archived <box.cassette@gmail.com>
2021-03-25 11:44:04 +0900

    Fix echo

--- run.bat
+++ run.bat
@@ -3,35 +3,18 @@ chcp 65001
 TITLE VLIVE-BACKUP-BOT
 CLS
 
-SET T1=기존 가상환경을 정리합니다.
-SET T2=가상환경을 생성합니다.
-SET T3=의존 프로그램을 설치합니다.
-SET T4=의존프로그램 설치를 완료했습니다.
-SET T5=프로그램을 실행합니다.
-
+ECHO LOADING...
 IF EXIST venv (
-ECHO %T1%
-ECHO.
 rmdir /S /Q venv
 )
 
-ECHO %T2%
-ECHO.
 python -m venv venv
 
-ECHO %T3%
-ECHO.
-venv\Scripts\python -m pip install --upgrade pip
-venv\Scripts\python -m pip install -r requirements.txt
+venv\Scripts\python -m pip install -q -q -q --upgrade pip
+venv\Scripts\python -m pip install -q -q -q -r requirements.txt
 
-ECHO.
-ECHO %T4%
-ECHO.
-
-ECHO %T5%
-ECHO.
 TIMEOUT /t 1 > nul
-cls
+CLS
 
 venv\Scripts\python core.py
 PAUSE
